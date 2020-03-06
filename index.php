5687379209fad7a4495ce91be6ac3cf969901e23
sergey42osn <sergey42osn@gmail.com>
2020-03-06 15:50:20 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Создание XML файла</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+</head>
+<body>
+	<div class="con">
+		<div class="row">
+			<div class="col">
+				<h1 class="aligen_centr">Создание файла XML онлайн</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<div class="col-con">
+					<div class="col-con-box aligen_centr">
+						<div class="col-con-box_h">
+							<h3 class="aligen_centr box_h_3">Введите адрес сайта</h3>
+						</div>
+						<div class="col-con-box_form">
+							<form action="parser.php">
+								<div class="box_form_input">
+									<input id="domain" class="domain" type="text" required="" placeholder="Введите адрес сайта">
+								</div>
+								<div class="box_form_input">
+									<input type="submit" value="Отправить">
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>				
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
---
+++ parser.php
@@ -0,0 +1,56 @@
+﻿<?php
+
+	echo "333";
+
+	require_once 'function.php';
+	require_once 'simple_html_dom.php';
+
+	set_time_limit(15);
+
+	if (!isset($_GET["val"]) || $_GET["val"] == '' ) {
+		echo 'Нет данных';
+		return;
+	}
+
+	$domain = trim($_GET["val"]);
+
+	if (!file_exists('sitemap.txt')) {
+		$xml = 'sitemap.txt';
+		file_put_contents($xml, '');
+	}
+
+	$file = 'sitemap.txt';
+	$res = file($file);
+	$count = count($res);
+
+	var_dump(count($res));
+
+	if ($count < 1) {
+		$href_f = $domain;
+	}else{
+		$href_f = $res[$count -1];
+	}
+
+	echo $href_f.'<br>';
+
+	$html = getHome($domain);
+	//echo $html;
+	$html = str_get_html($html);
+	$a = $html->find("a");
+
+	foreach ($a as $key => $value) {
+		$href = $value->href;
+		$path_info = pathinfo($value->href);
+
+		if (strlen($path_info["dirname"]) <= 1) {
+			$href = $domain;
+		}
+		elseif ($path_info["basename"] == '' || strlen($path_info["basename"]) == 1) {
+			return;
+		}
+
+		var_dump($path_info);
+		writeHrefToFile($href);
+	}
+
+?>
